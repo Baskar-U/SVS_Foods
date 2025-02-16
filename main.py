1551b9615627bb08961a26392d5d0b3b7312777d
Baskar U <155171674+Baskar-U@users.noreply.github.com>
2025-02-17 00:26:56 +0530

    main.py

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ app = Flask(__name__)
 
 # Temporary user session storage
 user_data = {}
+service_account_info = {
+  "type": "service_account",
+  "project_id": "svs-foods",
+  "private_key_id": "89c2710c0e3f887afa4afa58b1415c8dfa6a3c21",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC1kYSyHm2I9R+L\nY5noYMeLraeCRxBUJAqJuJkKUM2KQQVH9e92Wq+doIslAopqCkUp9SRytyQGPteB\nHzykGylPr8eTNQ3kV61pVm0bSlcqmYXwYgb3PzKzkLJDyZTjW1RzgP+OLPj5DI6l\nmIWpEd1BC62kICiDoZTDuOT9Vp8RsUPNLi4VdxpqgWB9WoK7OORCL3rLexZF8+WE\nIQAv1zCpkvYbbeq/51iUPWhRmjOMS7EzCuwf6FKbDXIVVCl47n0w72ztpruMmt3I\n3EPG3dFgnWTq8H3Thc4lrDxFNFpj+UzofiJFlk/YKjb1FX8KrNdh83tDCrEQaeTf\n+nTxLNszAgMBAAECggEARq3MWWf13KwK6QBPL8HIu8tQGysdpJtZDM5yqKzWrhxs\neVw3qLtZhIMz1ybbibbQwKoAU7WODO5WhpmJyhoWpUH4bwnmi/5Db29ozohMUTVt\nCaunKYUM8BCCDFhCoJRzVfkfwhEopA4q0zSBx1RAbCsnH24zRzZVijEF1sZq6oAO\nVFLtDjJYNWmeO6nY59VXxM9gzIJfL9fPDClaZMNNmgURMKGlWXZtCAefA7l1m5Ya\n7q5Y/p8lvZoj2jjkXMkzuaIqtY0Ids5DF/0vf2PSgGbSV1UzN1OhY5yV0G/km57L\nntju0EXhCHnPVd7FynS3aKdr45rjQzTbocELAvluYQKBgQDbJqvf25VK7+OFRLO1\nsiL70Y4JReuDJ+PmolS6rygdhaZ2abviFAA8WCvpaaPvltL85e+HzmG21UEgrQ33\nr5oczaTI4JaTuAC7VEasszCwNSLKmvrib6L4IW4u+619WFNT/mlvjSJ7JF8oehQu\nz2bWmNU+B496IW+IJL4hgQQaoQKBgQDUGRuNMpPsEftMQ6dfSMX0ntvmQaOqzBGu\nvDlaYPEzIK+Z5xoJ4nVoQ06NJZmunLPWqsm+K1zKUOkEhd0yFfBUnmrDcT88+u6t\niKA2RX2EHBFBlDwqWWBT4mAlP/TnRpaKpEi2GaLyGGrpPCRIXffjrmmZam+UqBTp\nHt+T4C6ZUwKBgDIrQS+HGj9EeYZ5utY+iImzaFuxGQA2cpLK2LDABmPzw67Z97wC\nriciXDMcDv57PaPNk684TOBJI5ayZz8rgRFU9oNR/sakWe6IlYFVqDC+iU9ueT67\nJci+BnZVAVtvi+vvkv2JhrpAElS21cX9juJTrIV9v5fK7bhyCOCso17hAoGAHItU\nfeg0qCyzWUwxlk1A9u76QT7MPAxCxPVn8pXhYDQh7eCATJxBmMqWI8DPsgQ838MU\na5eCYWgKNCWf40G4mmrHi44OKLLMtGYSybsIbWMpn13HyxtEL4Xj0p+M4iNJ11bM\nwlUo7NQEb2c8qtCar/WlGJSOd783MTiKdmhRXSkCgYEAzyHE/zCL84bD/Py9eyGR\nQ0D2MBSJcZb6NYKK3/m12hX22A4l4iVR64s3hzfA4qeULL7CM3C664GRYFpES/dr\n9MOGOYxMSxwUmXsniet90V8F0zj3UnVB7zBk5YLewfYH2EjwWm6OC5Phb9y2uTPh\npKg9TkLkZzc/j3+cmEBT+oM=\n-----END PRIVATE KEY-----\n",
+  "client_email": "svs-foods@svs-foods.iam.gserviceaccount.com",
+  "client_id": "100047334838084656784",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/svs-foods%40svs-foods.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
 
 # WhatsApp API Credentials
 ACCESS_TOKEN = "EAAOAi7h03l8BO349XrWMuMyuwbEL1g316QLHX3Ph3JZCPKVczIITZBtBuxkv4BZBK269Ck38L1IuE2uoZBPi7yP1aNXfCpP2Ps0vHBn5HYXFTEk9aZCvrmcCjbjntObrwvP6k7BKIojGtjJWqeafwp6NK80NHMEAccKIgJPuTvDi5sEZBfFWJB5pSq3wsNDfKuYXleRvgMiF7f9B7M0lKzjPh1KiIZD"
@@ -14,12 +27,12 @@ PHONE_NUMBER_ID = "550908254774273"
 VERIFY_TOKEN = "svsfoods"
 
 # Google Sheets API Configuration
-SERVICE_ACCOUNT_FILE = "credsvs.json"  # Replace with your JSON key file
+# Replace with your JSON key file
 SHEET_ID = "185XRA23EbkqWg2E_QBXFwzFjsnsS6Rh9ksiFlU8LCdo"  # Replace with your Google Sheet ID
 SHEET_NAME = "SVS_Foods"  # Sheet Name
 
 # Authenticate Google Sheets API
-creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=["https://www.googleapis.com/auth/spreadsheets.readonly"])
+creds = Credentials.from_service_account_file(service_account_info, scopes=["https://www.googleapis.com/auth/spreadsheets.readonly"])
 client = gspread.authorize(creds)
 
 def fetch_product_data():
